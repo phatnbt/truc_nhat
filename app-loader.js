13849429a2933213cf9553ad155b2a2b45b87496
phatnbt <108578473+phatnbt@users.noreply.github.com>
2026-08-19 21:08:57 +0700

    Load mobile install guide

--- app-loader.js
+++ app-loader.js
@@ -12,7 +12,8 @@ const parts = [
   "./home-enhancements.js?v=20260819-6",
   "./app-start.js?v=20260819-6",
   "./notification-enhancements.js?v=20260819-8",
-  "./today-calendar.js?v=20260819-10"
+  "./today-calendar.js?v=20260819-10",
+  "./mobile-install-guide.js?v=20260819-11"
 ];
 
 for (const src of parts) {
