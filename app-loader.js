3e1ddc4d5015b3c9bb35b878ab85ad56225df6e4
phatnbt <108578473+phatnbt@users.noreply.github.com>
2026-08-19 19:15:54 +0700

    bump landing modules

--- app-loader.js
+++ app-loader.js
@@ -1,15 +1,15 @@
-import { createP708SecureEngine } from "./p708-secure-sync-engine.js?v=20260819-2";
+import { createP708SecureEngine } from "./p708-secure-sync-engine.js?v=20260819-3";
 
 globalThis.createP708SecureEngine = createP708SecureEngine;
 
 const parts = [
-  "./app-core1.js?v=20260819-2",
-  "./app-core2.js?v=20260819-2",
-  "./app-actions1.js?v=20260819-2",
-  "./app-actions2.js?v=20260819-2",
-  "./app-dashboard.js?v=20260819-2",
-  "./app-render.js?v=20260819-2",
-  "./app-start.js?v=20260819-2"
+  "./app-core1.js?v=20260819-3",
+  "./app-core2.js?v=20260819-3",
+  "./app-actions1.js?v=20260819-3",
+  "./app-actions2.js?v=20260819-3",
+  "./app-dashboard.js?v=20260819-3",
+  "./app-render.js?v=20260819-3",
+  "./app-start.js?v=20260819-3"
 ];
 
 for (const src of parts) {
