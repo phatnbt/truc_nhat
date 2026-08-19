e80f31116f9458bda8aa6ed2be5d81b34aba69cb
phatnbt <108578473+phatnbt@users.noreply.github.com>
2026-08-19 19:45:57 +0700

    remove App Check config

--- app-check-config.js
+++
@@ -1,4 +0,0 @@
-// Public reCAPTCHA v3 site key for Firebase App Check.
-// Leave blank until the web app is registered in Firebase Console > Security > App Check.
-// This key is safe to ship to the browser. Keep the reCAPTCHA secret key only in Firebase/reCAPTCHA Console.
-export const APP_CHECK_SITE_KEY = "";
